18a52f0b01ca28fccdf5dfada645dfa758a1260d
JakeBloor <jakebloor@outlook.com>
2023-11-26 18:25:43 +0000

    Added Unit tests.

--- FizzBuzz.Tests/GlobalUsings.cs
+++ FizzBuzz.Tests/GlobalUsings.cs
@@ -1,3 +1,5 @@
 global using Xunit;
 global using FluentAssertions;
 global using FizzBuzz;
+global using FizzBuzz.Interfaces;
+global using FizzBuzz.Services;
---
+++ FizzBuzz.Tests/Services/FizzBuzzerTests.cs
@@ -0,0 +1,67 @@
+﻿namespace FizzBuzz.Tests;
+
+public class FizzBuzzerTests
+{
+    private readonly IFizzBuzzer _fizzBuzzer;
+
+    public FizzBuzzerTests()
+    {
+        _fizzBuzzer = new FizzBuzzer();
+    }
+
+    [Fact]
+    public void FizzBuzz_ShouldHaveLenthOf100()
+    {
+        var results = _fizzBuzzer.FizzBuzz();
+        results.Length.Should().Be(100);
+    }
+
+    [Fact]
+    public void FizzBuzz_ShouldFizzAt3()
+    {
+        var results = _fizzBuzzer.FizzBuzz();
+        results[2].Should().Be("Fizz");
+    }
+
+    [Fact]
+    public void FizzBuzz_ShouldBuzzAt5()
+    {
+        var results = _fizzBuzzer.FizzBuzz();
+        results[4].Should().Be("Buzz");
+    }
+
+    [Fact]
+    public void FizzBuzz_ShouldFizzBuzzAt15()
+    {
+        var results = _fizzBuzzer.FizzBuzz();
+        results[14].Should().Be("FizzBuzz");
+    }
+
+    [Fact]  
+    public void FizzBuzz_ShouldFizzBuzzAt53()
+    {
+        var results = _fizzBuzzer.FizzBuzz();
+        results[52].Should().Be("FizzBuzz");
+    }
+
+    [Fact]
+    public void FizzBuzz_ShouldFizzAt23()
+    {
+        var results = _fizzBuzzer.FizzBuzz();
+        results[22].Should().Be("Fizz");
+    }
+
+    [Fact]
+    public void FizzBuzz_ShouldBuzzAt25()
+    {
+        var results = _fizzBuzzer.FizzBuzz();
+        results[24].Should().Be("Buzz");
+    }
+
+    [Fact]
+    public void FizzBuzz_ShouldReturnNumberAsString()
+    {
+        var results = _fizzBuzzer.FizzBuzz();
+        results[0].Should().Be("1");
+    }
+}
